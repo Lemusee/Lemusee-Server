1f9779cc8821c9e5e30dda2263ba993f8c739479
wonseok <tkddls23@konkuk.ac.kr>
2023-01-02 20:16:35 +0900

    feat: corsConfit 설정

--- src/main/java/com/lemusee/lemusee_prj/config/CorsConfig.java
+++ src/main/java/com/lemusee/lemusee_prj/config/CorsConfig.java
@@ -5,15 +5,17 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import java.util.List;
+
 @Configuration
 public class CorsConfig {
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.addAllowedOriginPattern("*");
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://lemusee.site","https://prod.lemusee.site"));
         configuration.addAllowedHeader("*");
-        configuration.addAllowedMethod("*");
+        configuration.setAllowedMethods(List.of("GET","POST", "PUT", "DELETE"));
         configuration.setAllowCredentials(true);
 
         source.registerCorsConfiguration("/**", configuration);
